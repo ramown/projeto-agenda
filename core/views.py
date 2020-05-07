650f842b4e9b05f91d7f0ce131b35afc4d9ca374
ramown <emmanuelrlima@gmail.com>
2020-05-07 11:46:52 -0300

    Implementado login, logout e autenticação

--- core/views.py
+++ core/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+# def index(request):
+# 	return redirect('/agenda/')
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+	usuario = request.user
+	eventos = Evento.objects.filter(usuario=usuario)
+	#eventos = Evento.objects.all()
+	dados = {'eventos': eventos}
+	return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def logout_user(request):
+	logout(request)
+	return redirect('/')
+
+
+def login_user(request):
+	return render(request, 'login.html')
+
+def submit_login(request):
+	if request.POST:
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		usuario = authenticate(username=username, password=password)
+		if usuario is not None:
+			login(request, usuario)
+			return redirect('/')
+		else:
+			messages.error(request, "Usuário ou senha incorreto(s)")
+
+	return redirect('/')
