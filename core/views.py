b3993820ba993028123424a745c13bedeb6d1b3e
ramown <emmanuelrlima@gmail.com>
2020-05-07 12:18:28 -0300

    Implementado formulario para inserção de dados

--- core/views.py
+++ core/views.py
@@ -18,6 +18,30 @@ def lista_eventos(request):
 	return render(request, 'agenda.html', dados)
 
 @login_required(login_url='/login/')
+def evento(request):
+	usuario = request.user
+	evento = Evento.objects.filter(usuario=usuario)
+	#eventos = Evento.objects.all()
+	dados = {'evento': evento}
+	return render(request, 'evento.html', dados)
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+	if request.POST:
+		titulo = request.POST.get('titulo')
+		data_evento = request.POST.get('data_evento')
+		descricao = request.POST.get('descricao')
+		usuario = request.user
+		Evento.objects.create(
+			titulo=titulo, 
+			data_evento=data_evento, 
+			descricao=descricao, 
+			usuario=usuario)
+		return redirect('/')
+
+
+
+@login_required(login_url='/login/')
 def logout_user(request):
 	logout(request)
 	return redirect('/')
